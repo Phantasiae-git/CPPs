4d851d8a88a7b716e40d24a6dee8659a3dc89c50
Phantasiae <phantasiae.flr@gmail.com>
2025-06-09 18:26:04 +0100

    DONEEEE

--- cpp09/ex02/PMergeMe.hpp
+++ cpp09/ex02/PMergeMe.hpp
@@ -20,7 +20,7 @@ std::vector<int> jacobstahl(int ps, int size)//organic jacobstahl (+regression),
         order.push_back(3);
     int prev= 3;
     int prev1= 1;
-    std::cout << "order size= " << order.size() << " elements: " << elements << std::endl;
+    //std::cout << "order size= " << order.size() << " elements: " << elements << std::endl;
     while(order.size()<elements) {
         int back=prev;
         while(--back>prev1 && order.size()<elements)
@@ -31,10 +31,10 @@ std::vector<int> jacobstahl(int ps, int size)//organic jacobstahl (+regression),
         prev1=prev;
         prev=next;
     }
-            std::cout << "prev= " << prev << " elements: " << elements << std::endl;
+            //std::cout << "prev= " << prev << " elements: " << elements << std::endl;
     if(prev>(int)elements+1)
     {
-        std::cout << "prev= " << prev << " elements: " << elements << std::endl;
+        //std::cout << "prev= " << prev << " elements: " << elements << std::endl;
         std::vector<int>::iterator ceil_it = order.begin();
         while ((*ceil_it)!=prev)
             ceil_it++;
@@ -47,13 +47,13 @@ std::vector<int> jacobstahl(int ps, int size)//organic jacobstahl (+regression),
         while (++ceil_it!=order.end())
             (*ceil_it)-=diff;
     }
-    std::vector<int>::iterator it=order.begin();
-    std::cout << "--------JACOBSTAHL: ";
-    while (it!=order.end())
-    {
-        std::cout << *it << " ";
-        it++;
-    }
+    // std::vector<int>::iterator it=order.begin();
+    // std::cout << "--------JACOBSTAHL: ";
+    // while (it!=order.end())
+    // {
+    //     std::cout << *it << " ";
+    //     it++;
+    // }
     return order;
 }
 
@@ -71,23 +71,21 @@ template <typename T, typename itr> void insert_m(itr s, itr e, int idx, T &main
 template <typename T>  void do_insertion(T &cont, int pair_size, int pairs)
 {
     typedef typename T::iterator itr;
-    if(pair_size==1)
-        std::cout << "AAAAAAAAAAAAAAAAAAAAAAAAAA" << std::endl;
     int b_nbr=pairs/2;
     if(pairs%2==1)
         b_nbr++;
-    std::cout << "pairs: "<< pairs << " b nbr: " << b_nbr << std::endl;
+    //std::cout << "pairs: "<< pairs << " b nbr: " << b_nbr << std::endl;
     T pend(b_nbr*(pair_size));
     T main(cont.size()-pend.capacity());
 
-        std::cout << "recursion level "<< pair_size << std::endl;
-    itr a=cont.begin();
-    while (a!=cont.end())
-    {
-        std::cout << *a << " ";
-        a++;
-    }
-    std::cout << std::endl;
+    //     std::cout << "recursion level "<< pair_size << std::endl;
+    // itr a=cont.begin();
+    // while (a!=cont.end())
+    // {
+    //     std::cout << *a << " ";
+    //     a++;
+    // }
+    // std::cout << std::endl;
     itr b_s=cont.begin();
     itr paste_p=pend.begin();
     itr paste_m=main.begin();
@@ -95,14 +93,14 @@ template <typename T>  void do_insertion(T &cont, int pair_size, int pairs)
     for(int i=0; i<b_nbr; i++)
     {
         itr b_e= b_s + pair_size;//not included/ past the end of b
-        std::cout << "b_s: " << *b_s << " b_e: " << *(b_e-1) << " pair size :" << pair_size << " pend capacity: " << pend.capacity() << std::endl;
+        //std::cout << "b_s: " << *b_s << " b_e: " << *(b_e-1) << " pair size :" << pair_size << " pend capacity: " << pend.capacity() << std::endl;
         std::copy(b_s, b_e, paste_p);
         if(!(pairs%2==1 && i==(b_nbr-1)))
         {
-            std::cout << "HELLOOOOOOOOOOOOOOOOOOOOOOO " << i << std::endl;
+            //std::cout << "HELLOOOOOOOOOOOOOOOOOOOOOOO " << i << std::endl;
             b_s=b_e+ pair_size;//start (included) of the next b is also end (not included) of the next a
             std::copy(b_e, b_s, paste_m);//b_e=a_s, b_s=a_e
-            std::cout << "b_s: " << *(b_s-1) << " b_e: " << *b_e << " pair size :" << pair_size << " pend capacity: " << pend.capacity() << std::endl;
+            //std::cout << "b_s: " << *(b_s-1) << " b_e: " << *b_e << " pair size :" << pair_size << " pend capacity: " << pend.capacity() << std::endl;
             paste_m+=pair_size;
         }
         paste_p+=pair_size; 
@@ -116,23 +114,23 @@ template <typename T>  void do_insertion(T &cont, int pair_size, int pairs)
         paste_m++;
         pool_end++;
     }
-    itr idk = pend.begin();
-    std::cout << "pend: " << std::endl;
-    while (idk!=pend.end())
-    {
-        std::cout << *idk << " ";
-        idk++;
-    }
-    std::cout << std::endl;
+    // itr idk = pend.begin();
+    // std::cout << "pend: " << std::endl;
+    // while (idk!=pend.end())
+    // {
+    //     std::cout << *idk << " ";
+    //     idk++;
+    // }
+    // std::cout << std::endl;
 
-    idk = main.begin();
-    std::cout << "main: " << std::endl;
-    while (idk!=main.end())
-    {
-        std::cout << *idk << " ";
-        idk++;
-    }
-    std::cout << std::endl;
+    // idk = main.begin();
+    // std::cout << "main: " << std::endl;
+    // while (idk!=main.end())
+    // {
+    //     std::cout << *idk << " ";
+    //     idk++;
+    // }
+    // std::cout << std::endl;
     cont= main;
     std::vector<int> order=jacobstahl(pair_size, pend.size());
     order.erase(order.begin());
@@ -140,16 +138,16 @@ template <typename T>  void do_insertion(T &cont, int pair_size, int pairs)
     itr b_e=b_s+pair_size;
 
     insert_m(b_s, b_e, 0, cont);//insert b1 in main
-    idk = cont.begin();
-    std::cout << "main (cont): " << std::endl;
-    while (idk!=cont.end())
-    {
-        std::cout << *idk << " ";
-        idk++;
-    }
+    // idk = cont.begin();
+    // std::cout << "main (cont): " << std::endl;
+    // while (idk!=cont.end())
+    // {
+    //     std::cout << *idk << " ";
+    //     idk++;
+    // }
     std::vector<int>::iterator order_it=order.begin();
 
-    std::cout << "INSERTION: " << std::endl;
+    //std::cout << "INSERTION: " << std::endl;
 
     std::vector<int> idxs;
     int count=1;
@@ -165,57 +163,57 @@ template <typename T>  void do_insertion(T &cont, int pair_size, int pairs)
         idxs.push_back(count);
 
     //PRINTS
-    std::vector<int>::iterator idxs_it= idxs.begin();//EACH INDEX IS LEFTMOST ELEMENT OF A X = index * pair_size, so index 3 means ax actually starts at pair_size*3
-    while (idxs_it!=idxs.end())
-    {
-        std::cout << "index: " << *idxs_it << std::endl;
-        idxs_it++;
-    }
+    // std::vector<int>::iterator idxs_it= idxs.begin();//EACH INDEX IS LEFTMOST ELEMENT OF A X = index * pair_size, so index 3 means ax actually starts at pair_size*3
+    // while (idxs_it!=idxs.end())
+    // {
+    //     //std::cout << "index: " << *idxs_it << std::endl;
+    //     idxs_it++;
+    // }
 
 
     while (order_it!=order.end())
     {
-        idk = pend.begin();
-        std::cout << "pend: " << std::endl;
-        while (idk!=pend.end())
-        {
-            std::cout << *idk << " ";
-            idk++;
-        }
-        std::cout << std::endl;
+        // idk = pend.begin();
+        // std::cout << "pend: " << std::endl;
+        // while (idk!=pend.end())
+        // {
+        //     std::cout << *idk << " ";
+        //     idk++;
+        // }
+        //std::cout << std::endl;
         b_e=pend.begin()+pair_size*(*order_it);
-        std::cout << "hello " << *order_it << " " << *(b_e-1) << std::endl;
+        //std::cout << "hello " << *order_it << " " << *(b_e-1) << std::endl;
         b_s=b_e-pair_size;
-        std::cout <<"b_s: "<< *b_s <<std::endl;
+        //std::cout <<"b_s: "<< *b_s <<std::endl;
 
 
 
         int pool_end= idxs[(*order_it)-1];
-        std::cout <<"pool end: "<< pool_end <<std::endl;
+        //std::cout <<"pool end: "<< pool_end <<std::endl;
         itr ins_pos=std::lower_bound(small_main.begin(), small_main.begin()+pool_end, *(b_e-1));
         int ins_idx=std::distance(small_main.begin(), ins_pos)*pair_size;
-        std::cout << "FINAL INSERTION INDEX: " << ins_idx << std::endl;
+        //std::cout << "FINAL INSERTION INDEX: " << ins_idx << std::endl;
         insert_m(b_s, b_e, ins_idx, cont);
 
-        itr small_main_it =small_main.begin();
-        std::cout << "small main: " << std::endl;
-        while(small_main_it!=small_main.end())
-        {
-            std::cout << *small_main_it << std::endl;
-            small_main_it++;
-        }
+        //itr small_main_it =small_main.begin();
+        // std::cout << "small main: " << std::endl;
+        // while(small_main_it!=small_main.end())
+        // {
+        //     std::cout << *small_main_it << std::endl;
+        //     small_main_it++;
+        // }
 
         small_main.insert(ins_pos, *(b_e-1));
-        idk = cont.begin();
-        std::cout << "main (cont): " << std::endl;
-        while (idk!=cont.end())
-        {
-            std::cout << *idk << " ";
-            idk++;
-        }
-        std::cout << "ins_idx/pair_size=" << ins_idx/pair_size << std::endl;
-        idxs_it=idxs.begin();//only the As from ins_idx on moved forward (by pair size)
-        std::cout << "UPDATED INDEXES: " << std::endl;
+        // idk = cont.begin();
+        // std::cout << "main (cont): " << std::endl;
+        // while (idk!=cont.end())
+        // {
+        //     std::cout << *idk << " ";
+        //     idk++;
+        // }
+        //std::cout << "ins_idx/pair_size=" << ins_idx/pair_size << std::endl;
+        std::vector<int>::iterator idxs_it= idxs.begin();//only the As from ins_idx on moved forward (by pair size)
+        //std::cout << "UPDATED INDEXES: " << std::endl;
         while(idxs_it!=idxs.end())//updating indexes of As that moved
         {
             if(*idxs_it>=(ins_idx/pair_size))
@@ -225,7 +223,7 @@ template <typename T>  void do_insertion(T &cont, int pair_size, int pairs)
         while (idxs_it!=idxs.end())
         {
             (*idxs_it)++;
-            std::cout << "index number " << std::distance(idxs.begin(), idxs_it) << ": " << *idxs_it << std::endl;
+            //std::cout << "index number " << std::distance(idxs.begin(), idxs_it) << ": " << *idxs_it << std::endl;
             idxs_it++;
         }
         
@@ -241,17 +239,17 @@ template <typename T> void FJ(T &cont, int pair_size)
     int pairs=cont.size()/pair_size;
     // int pairs_f=cont.size()/(pair_size/2);//this is a surpirse tool that can help us later! ;) because i messed up the recursion
 
-    if(pairs<2)
+    if(pairs<1)
         return;
     typedef typename T::iterator itr;
-
+    //std::cout << "pairs: " <<pairs << std::endl;
     itr start = cont.begin();
 
     for(int i=0; i<pairs; i++)
     {
         itr n1 = start + pair_size/2-1;
         itr n2 = start + pair_size-1;
-        std::cout << "start: "<< *start << " n1: "<< *n1 << " n2: "<< *n2 << std::endl;
+        //std::cout << "start: "<< *start << " n1: "<< *n1 << " n2: "<< *n2 << std::endl;
         if((*n1)>(*n2))
         {
             T tmp(n2-(pair_size/2-1), n2+1);
